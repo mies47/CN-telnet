d2573273de4817b8494e3562021d8da4d5c71cd1
esrafiliyanm <esrafiliyanm@gmail.com>
2021-05-25 15:45:06 +0430

    feat(client): :sparkles: Add port description

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import os
 import socket
 import sys
 import ipaddress
+import json
 
 ENCODING = 'utf-8'
 CHUNK_SIZE = 1024
+PORTS_FILE = open('ports.json', 'r')
+PORTS_DIC = json.load(PORTS_FILE)
+PORTS_FILE.close()
+
 
 def main():
     'Main method'
@@ -14,6 +19,7 @@ def createSocket(host:str, port:int):
     '''Creates a TCP socket with given host and port
        Returns if the socket creation was successful
        and created socket/exception'''
+
     try:
         created_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         created_socket.connect((socket.gethostbyname(host), port))
@@ -27,6 +33,7 @@ def createSocket(host:str, port:int):
 def sendMessage(given_socket:socket, message:str):
     '''Send the message into the given socket
        Returns total bytes sent'''
+
     encoded_message = message.encode(ENCODING)
     message_length = len(encoded_message)
     total_sent = 0
@@ -48,6 +55,7 @@ def sendFile(given_socket:socket, path:str):
        Send file name
        Send content of file as binary data
        Returns total bytes sent or error'''
+
     with open(path, 'rb') as f:
         fileName = os.path.basename(path)
         given_socket.send(b'\xff\x00') #Sending some random bits. I hope it works.
@@ -74,20 +82,25 @@ def sendFile(given_socket:socket, path:str):
 def check_port(host_ip:str, port:int):
     '''Call create_socket check if connection was refused
        Returns if connection was refused'''
+    
     res, s = createSocket(host_ip, port)
     if type(s) == socket.socket:
         s.close()
     return res
 
 def scan_ports(host_ip_start:str, host_ip_end:str):
+    '''Scans over the range of IPs given through port 0 to 1024
+       Calls check_port to see if connection is refused
+       Prints the open ports of different IPs'''
+    
     ip_start = ipaddress.IPv4Address(host_ip_start)
     ip_end = ipaddress.IPv4Address(host_ip_end)
 
+    print('IP\t\tPort\tName\t\tComment')
     while ip_start < ip_end:
         for port in range(1024):
-            is_port_open = check_port(str(ip_start), port)
-            if is_port_open:
-                print(f'{ip_start}\t{port}\t{is_port_open}')
+            if check_port(str(ip_start), port):
+                print(f'{ip_start}\t{port}\t{PORTS_DIC[str(port)]["name"]}\t\t{PORTS_DIC[str(port)]["comment"]}')
         ip_start += 1
 
 
